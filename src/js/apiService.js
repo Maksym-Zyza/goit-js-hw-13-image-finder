63942484402e7f94a80e217e24dbf2f2aacd9f27
Maksym Zyza <mmziza88@gmail.com>
2021-01-10 13:37:30 +0200

    оформлено

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,19 +1,5 @@
 const apiKey = '19268663-7f88b2c67b4246c5b4cd88041';
 
-// export default {
-//     query: '',
-//     page: 1,
-//     perPage: 12,
-  
-//     async getCards() {
-//         const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=${this.perPage}&key=${apiKey}`;
-//         const res = await fetch(url);
-//         const cards = res.json();
-//         return console.log(cards);
-//     },
-// }
-
-
 export default {
   searchQuery: '',
   page: 1,
